fe1ff0d1d88bbe8c5b9d89b9529a0b7e1f76ff8a
Mohit Yadav <mahiyad2005@gmail.com>
2025-11-01 18:28:37 +0530

    Implement high score tracking in Scorecard
    Added high score functionality with file storage.

--- scorecard.py
+++ scorecard.py
@@ -6,22 +6,30 @@ FONT = ("Arial", 15, "normal")
 class Scorecard(Turtle):
     def __init__(self):
         super().__init__()
+        self.score = 0
+        with open("./data.txt") as file:
+            self.high_score = int(file.read())
         self.hideturtle()
         self.color("white")
         self.penup()
         self.setposition(x=0, y=POSITION)
-        self.score = 0
         self.update_scoreboard()
         
     def update_scoreboard(self):
         self.clear()
-        self.write(arg=f"Score : {self.score}", align= ALIGNMENT, font=FONT)
+        if self.score > self.high_score:
+            self.high_score = self.score
+        self.write(arg=f"Score:{self.score}    High Score:{self.high_score}", align= ALIGNMENT, font=FONT)
+        print(self.high_score)
+        
+    def reset(self):
+        if self.score>=self.high_score:
+            self.high_score=self.score
+            with open("./data.txt", "w") as file:
+                file.write(str(self.high_score))
+        self.score = 0
+        self.update_scoreboard()
         
     def increase_score(self):
         self.score += 1    
         self.update_scoreboard()
-        
-    def game_over(self):
-        self.setposition(0,0)
-        self.write(arg="GAME OVER", align= ALIGNMENT, font=FONT)
-        
